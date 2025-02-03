7938d9401b3765e2d5f5c0df6885d49ea70842a0
sirius829 <matthew.egan.work@gmail.com>
2025-02-04 00:27:27 +0100

    feat: add Receipt and Item data structures to main.go

--- main.go
+++ main.go
@@ -20,6 +20,19 @@ func main() {
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
+type Item struct {
+	ShortDescription string `json:"shortDescription"`
+	Price            string `json:"price"`
+}
+
+type Receipt struct {
+	Retailer     string `json:"retailer"`
+	PurchaseDate string `json:"purchaseDate"`
+	PurchaseTime string `json:"purchaseTime"`
+	Items        []Item `json:"items"`
+	Total        string `json:"total"`
+}
+
 func processReceiptHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Process Receipt"))
 }
