49d8a7ff9713f7b8ec6cf033a7b4bdbe5f032a28
sirius829 <matthew.egan.work@gmail.com>
2025-02-04 00:25:09 +0100

    feat: add Gorilla Mux routing with placeholder handlers for API endpoints

--- main.go
+++ main.go
@@ -3,13 +3,27 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Receipt Processor Service"))
-	})
+	r := mux.NewRouter()
+	r.HandleFunc("/receipt/process", processReceiptHandler).Methods("POST")
+	r.HandleFunc("/receipt/{id}/points", getPointsHandler).Methods("GET")
+
+	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	w.Write([]byte("Receipt Processor Service"))
+	// })
 
 	log.Println("Server starting on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func processReceiptHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Process Receipt"))
+}
+
+func getPointsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Get Receipt Points"))
 }
